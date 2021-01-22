6a7542c93ac41df4f616f9f893d9425374b1cfea
Mohamed Bakr <mohabdelfattahbkr@gmail.com>
2021-01-22 21:52:56 +0500

    setup network repositories provider

--- Owange-Task/Network/NetworkManager/NetworkManager.swift
+++ Owange-Task/Network/NetworkManager/NetworkManager.swift
@@ -9,5 +9,8 @@ import Moya
 import Moya_ObjectMapper
 
 class NetworkManager: NSObject {
-
+    
+    lazy var repositoriesAPIProvider: MoyaProvider<RepositoriesAPI> = {
+        return  MoyaProvider<RepositoriesAPI>(plugins:([NetworkLoggerPlugin()]))
+    } ()
 }
